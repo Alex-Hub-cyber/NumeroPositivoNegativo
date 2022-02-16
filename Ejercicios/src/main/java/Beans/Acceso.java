f7a94ea54cd9e78bfa13658e38f98583b56360de
Alex-Hub-cyber <gl0430032020@unab.edu.sv>
2022-02-15 19:18:40 -0600

    Ejercicio
    E

---
+++ Ejercicios/src/main/java/Beans/Acceso.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Beans;
+
+import Services.INumero;
+
+/**
+ *
+ * @author user
+ */
+public class Acceso extends INumero{
+     
+ 
+}
---
+++ Ejercicios/src/main/java/Pojos/Numero.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Pojos;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author user
+ */
+public class Numero {
+     public static void main(String[] arg){
+      Scanner entradas = new Scanner(System.in); 
+        int numero1;
+    int numero2;
+
+    int NumeroPositivo = 0;
+    int NumeroNegativo = 0;
+    
+         System.out.println("IntroduzcaPrimer numero:");
+        numero1 = entradas.nextInt();
+
+        System.out.println("IntroduzcaSegundo numero");
+        numero2 = entradas.nextInt();
+           int[] arreglo = new int[2];
+        arreglo[0] = numero1;
+        arreglo[1] = numero2;
+       
+      
+        for (int a = 0;  a < arreglo.length; a++){
+
+            if (arreglo[a] < 0) {
+                NumeroNegativo++;
+//               
+            } else if (arreglo[a] > 0) {
+                NumeroPositivo++;
+//               
+            
+        }
+        
+         if(NumeroNegativo > 0) {
+            System.out.println("La cantidad de numeros negativos es " + NumeroNegativo);       
+        }      
+          if(NumeroPositivo>0){
+        System.out.println("La cantidad de numeros positivos es " +NumeroPositivo);
+     }
+}
+}
+}
